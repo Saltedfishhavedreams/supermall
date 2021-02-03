f2ee95506c16566c13a9781cf46b71ce314cf6f4
itcast <itheima@itcast.cn>
2021-02-03 13:51:21 +0800

    首页开发

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div id="tab-bar">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TabBar',
+}
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background-color: #f8f8f8;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 1px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+    <!-- router-link -->
+    <!-- <router-view></router-view> -->
+    <tab-bar>
+      <tab-bar-item path='/home'>
+      <!-- 在webpack中配置的别名 htmlDOM元素中使用别名需要添加 "~" 符号 -->
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category'>
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/shopcart'>
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile' activeColor='#ff8400'>
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+<script>
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+export default {
+    name: 'MainTabBar',
+    components: {
+      TabBar,
+      TabBarItem
+    }
+
+}
+</script>
+<style>
+
+</style>
